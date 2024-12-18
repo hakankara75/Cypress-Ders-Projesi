540553b9ed06ada71c43863b74f630598489e890
hakankara75 <hakanbatirhan@gmail.com>
2024-12-18 12:03:51 +0300

    v2

--- cypress/e2e/ders/ders07_before_after_then_text.cy.js
+++ cypress/e2e/ders/ders07_before_after_then_text.cy.js
@@ -1,29 +1,29 @@
-describe('before and after', () => {
-    before(() => {
-        cy.visit('/')
-        cy.log('web sitesine gidildi')
-
-    });
-
-    it('test1', () => {
+describe('before after', () => {
+   before(() => {
+    cy.visit('/')
+    cy.log('web sitesine gidildi')
+   });
+    it('test 1', () => {
+       
         cy.get('header > a > img').should('be.visible')
-        cy.log('web sitesi logosu görünür durumda')
+        cy.log('web sitesinde logonun görünür olduğu doğrulandı')
+
     });
 
-    it.only('test2', () => {
+    it.only('test 2', () => {
         cy.scrollTo('bottom')
         cy.get(':nth-child(1) > :nth-child(1) > .card-body > h5').as('elements')
         cy.get('@elements').should('have.text','Elements')
-        cy.get('@elements').then(($element)=>{ 
+        cy.get('@elements').then(($element) => {
             const textValue=$element.text()
-            cy.log('Sorgulanan elementin text değerinin '+ textValue +' olduğu doğrulandı')
+            cy.log('Sorgulanan elementin text değerinin '+ textValue + ' olduğu doğrulandı')
         })
     });
 
     after(() => {
         cy.visit('https://demoqa.com/automation-practice-form')
         cy.url().should('equal', 'https://demoqa.com/automation-practice-form')
-        cy.url().should('include', 'automation-practice-form')
+        cy.url().should('include','automation-practice-form')
         cy.log('Form linkine gidildi')
     });
 });
